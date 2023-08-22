ab1b48ce4eeaa3fb153224894c5d634764f59ff0
vitoriasaturnino <vim.saturnino@gmail.com>
2023-08-22 16:43:14 -0300

    build: initial commit

---
+++ src/main/java/br/gov/sp/fatec/backendjavaspring/BackendJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.backendjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/backendjavaspring/BackendJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.backendjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
